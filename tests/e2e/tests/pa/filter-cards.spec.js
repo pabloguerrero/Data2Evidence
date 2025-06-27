476dab0f090c91d128b61eda61f976698dd78834
mwaiyee <waiyee.man@data4life-asia.care>
2025-06-27 14:19:06 +0800

    change port

--- tests/e2e/tests/pa/filter-cards.spec.js
+++ tests/e2e/tests/pa/filter-cards.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   // step 1 - navigate to the portal
-  await page.goto('https://localhost:41100/portal');
+  await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').click();
   await page.locator('input[name="identifier"]').fill('admin');
   await page.locator('input[name="password"]').click();
