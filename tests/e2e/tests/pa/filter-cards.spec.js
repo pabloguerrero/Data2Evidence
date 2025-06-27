0b59f06df728958d4c37d181b43c828e163da1c5
mwaiyee <waiyee.man@data4life-asia.care>
2025-06-27 14:19:47 +0800

    edit test name

--- tests/e2e/tests/pa/filter-cards.spec.js
+++ tests/e2e/tests/pa/filter-cards.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('pa-filter-cards', async ({ page }) => {
   // step 1 - navigate to the portal
   await page.goto('https://localhost:443/portal');
   await page.locator('input[name="identifier"]').click();
