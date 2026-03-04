37e2a5f86129358b4c1c96773f31dd7911926e54
maggie-li-yd <maggie.li@data4life-asia.care>
2026-03-04 12:46:13 +0800

    add color options

--- ui/apps/vue-mri-ui-lib/src/components/StackBarChart.vue
+++ ui/apps/vue-mri-ui-lib/src/components/StackBarChart.vue
@@ -242,7 +242,10 @@ export default {
         // return ['#000080', '#6DAABDFF', '#CAECB3FF', '#F3CB04FF', '#E28027FF']
         // return ['#000080', '#3e92e0', '#c0ced1', '#fcd34b', '#f79409'] // colorblind safe, passes WCAG AA contrast requirement, with outline #595757
         // return ['#000080', '#f79409', '#3e92e0', '#FB9F9D', '#fcd34b'] //
-        return ['#56B4E9', '#0072B2', '#CC79A7', '#009E73', '#E69F00'] // not colorblind safe
+        // return ['#56B4E9', '#0072B2', '#CC79A7', '#009E73', '#E69F00'] // not colorblind safe
+        // return ['#56B4E9', '#009E73', '#D55E00', '#CC79A7', '#E69F00']
+        // return ['#0072B2', '#F0E442', '#009E73', '#56B4E9', '#E69F00'] // okabe-ito
+        return ['#E69F00', '#56B4E9', '#009E73', '#0072B2', '#D55E00', '#CC79A7'] // okabe-ito 2
 
         // return [
         //   '#212E52FF',
