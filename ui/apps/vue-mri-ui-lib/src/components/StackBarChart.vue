4d2d9e914b9fe6bdc38dc452301b1bc74ea45006
maggie-li-yd <maggie.li@data4life-asia.care>
2026-03-05 15:08:32 +0800

    add more color options

--- ui/apps/vue-mri-ui-lib/src/components/StackBarChart.vue
+++ ui/apps/vue-mri-ui-lib/src/components/StackBarChart.vue
@@ -230,7 +230,7 @@ export default {
         return ['#000080', '#3E92E0FF', '#FCD34BFF', '#F79409FF']
       } else {
         // Full palette for 5+ traces
-        // return ['#000080', '#144187', '#23718c', '#529f70', '#8bcb48', '#fee727']
+        // return ['#000080', '#144187', '#23718c', '#529f70', '#8bcb48', '#fee727'] // ggplot color in stackoverflow
         // return ['#000080', '#5d2085', '#9e3688', '#de4b8b', '#EC6E41', '#f5c747'] // option 1
         // return ['#000080', '#9e3688', '#de4b8b', '#EC6E41', '#f5c747'] // option 1
         // return ['#000080', '#540982', '#821d80', '#ab3478', '#d04d64', '#eb7100'] // option 2
@@ -241,12 +241,11 @@ export default {
         // return ['#6BBAE5FF', '#E3EEF4FF', '#000080', '#F9F5EAFF', '#81974CFF', '#553F31FF'] // r color palette 2
         // return ['#000080', '#6DAABDFF', '#CAECB3FF', '#F3CB04FF', '#E28027FF']
         // return ['#000080', '#3e92e0', '#c0ced1', '#fcd34b', '#f79409'] // colorblind safe, passes WCAG AA contrast requirement, with outline #595757
-        // return ['#000080', '#f79409', '#3e92e0', '#FB9F9D', '#fcd34b'] //
+        return ['#000080', '#f79409', '#3e92e0', '#FB9F9D', '#fcd34b'] // most recommended by leandro and Karthik
         // return ['#56B4E9', '#0072B2', '#CC79A7', '#009E73', '#E69F00'] // not colorblind safe
         // return ['#56B4E9', '#009E73', '#D55E00', '#CC79A7', '#E69F00']
         // return ['#0072B2', '#F0E442', '#009E73', '#56B4E9', '#E69F00'] // okabe-ito
-        return ['#E69F00', '#56B4E9', '#009E73', '#0072B2', '#D55E00', '#CC79A7'] // okabe-ito 2
-
+        // return ['#E69F00', '#56B4E9', '#009E73', '#0072B2', '#D55E00', '#CC79A7'] // okabe-ito 2
         // return [
         //   '#212E52FF',
         //   '#444E7EFF',
